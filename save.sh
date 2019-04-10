c933db7bbc293d9dbb1f625735a0692ae0f989f8
Eduardo Shanahan <contact@eduardoshanahan.com>
2019-04-10 17:13:15 +0100

    Some logging added

--- save.sh
+++ save.sh
@@ -1,7 +1,12 @@
 IMAGE_VERSION=$(cat version.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+echo 'Pushing to bitbucket'
 git push bitbucket
+echo 'Pushing to github'
 git push github
+echo 'Creating tag'
 if git tag $IMAGE_VERSION; then
+    echo 'Pushing tags to bitbucket'
     git push bitbucket --tags
+    echo 'Pushing tags to github'
     git push github --tags
 fi
