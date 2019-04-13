056f1c965edf9bc1c0b006d2da55be66695f5ccc
Eduardo Shanahan <contact@eduardoshanahan.com>
2019-04-13 07:29:27 +0100

    Edited saving script to force tag to be refreshed

--- save.sh
+++ save.sh
@@ -1,15 +1,18 @@
+echo 'Preparing version number'
 IMAGE_VERSION=$(cat version.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+echo 'New version is '$IMAGE_VERSION
+
 echo 'Pushing to bitbucket'
 git push bitbucket
+
 echo 'Pushing to github'
 git push github
-echo 'Checking tag'
-if [ ! $(git tag -l $IMAGE_VERSION) ]; then
-    echo 'Creating new tag'
-    git tag $IMAGE_VERSION
-    echo 'Pushing tags to bitbucket'
-    git push bitbucket --tags
-    echo 'Pushing tags to github'
-    git push github --tags
-fi
-    echo 'Tag already existing'
+
+echo 'Creating new tag '$IMAGE_VERSION
+git tag -f $IMAGE_VERSION
+
+echo 'Pushing tags to bitbucket'
+git push bitbucket -f --tags
+
+echo 'Pushing tags to github'
+git push github -f --tags
